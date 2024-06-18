925865963011c7d118f2e6e2871c0cf89d9e071e
MutebiRoy <roymbscit@gmail.com>
2024-06-18 05:17:40 -0700

    Initial commit

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+const authConfig = {
+	providers: [
+	  {
+		domain: process.env.CLERK_ISSUER_URL,
+		//domain: "https://hopeful-whale-77.clerk.accounts.dev",
+		applicationID: "convex",
+	  },
+	]
+  };
+
+export default authConfig;
---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{ hostname: "good-anteater-372.convex.cloud" }
+		],
+	},
+};
+
+export default nextConfig;
