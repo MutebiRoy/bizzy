911f020e1d3c8fad7ba6c1d05873382cf9af2841
MutebiRoy <roymbscit@gmail.com>
2024-06-24 07:04:18 -0700

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,21 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
-
-
-// See https://clerk.com/docs/references/nextjs/auth-middleware
-// for more information about configuring your Middleware
-export default clerkMiddleware({
-	// Allow signed out users to access the specified routes:
-	// publicRoutes: ["/"],
-});
-
-export const config = {
-	matcher: [
-		// Exclude files with a "." followed by an extension, which are typically static files.
-		// Exclude files in the _next directory, which are Next.js internals.
-		"/((?!.+\\.[\\w]+$|_next).*)",
-		// Re-include any files in the api or trpc folders that might have an extension
-		"/(api|trpc)(.*)",
-	],
-};
+import {
+	clerkMiddleware,
+	createRouteMatcher
+  } from '@clerk/nextjs/server';
+  
+  const isProtectedRoute = createRouteMatcher([
+	'/(.*)',
+  ]);
+  
+  export default clerkMiddleware((auth, req) => {
+	if (!auth().userId && isProtectedRoute(req)) {
+  
+	  // Add custom logic to run before redirecting
+  
+	  return auth().redirectToSignIn();
+	}
+  });
+  
+  export const config = {
+	matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  };
