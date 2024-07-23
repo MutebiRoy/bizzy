61b179a7e92ebfcbda6ac17740c32c063d92362f
MutebiRoy <roymbscit@gmail.com>
2024-07-23 00:17:21 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,9 @@ import { useTheme } from "next-themes";
 
 export default function Home() {
 	return (
-		<main className='m-5'>
-			<div className='flex overflow-y-hidden h-[calc(100vh-50px)] max-w-[1700px] mx-auto bg-left-panel'>
-				{/* Green background decorator for Light Mode */}
-				<div className='fixed top-0 left-0 w-full h-36 bg-green-primary dark:bg-transparent -z-30' />
+		<main className=''>
+			<div className='overflow-y-hidden h-screen mx-auto bg-left-panel'>
 				<LeftPanel />
-				<RightPanel />
 			</div>
 		</main>
 	);
