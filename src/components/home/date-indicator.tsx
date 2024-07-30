a11fa2320a90f7d582ad9f96f17c3d63993b9d16
MutebiRoy <roymbscit@gmail.com>
2024-07-29 20:40:12 -0700

    Update date-indicator.tsx

--- src/components/home/date-indicator.tsx
+++ src/components/home/date-indicator.tsx
@@ -8,7 +8,7 @@ type DateIndicatorProps = {
 const DateIndicator = ({ message, previousMessage }: DateIndicatorProps) => {
 	return (
 		<>
-			{!previousMessage || !isSameDay(previousMessage._creationTime, message._creationTime) ? (
+			{!previousMessage || !isSameDay(Number(previousMessage._creationTime), Number(message._creationTime)) ? (
 				<div className='flex justify-center'>
 					<p className='text-sm text-gray-500 dark:text-gray-400 mb-2 p-1 z-50 rounded-md bg-white dark:bg-gray-primary'>
 						{getRelativeDateTime(message, previousMessage)}
@@ -17,5 +17,6 @@ const DateIndicator = ({ message, previousMessage }: DateIndicatorProps) => {
 			) : null}
 		</>
 	);
+	
 };
 export default DateIndicator;
