2406b386681dcdfa8a5c65946d55eede2fc581ba
MutebiRoy <roymbscit@gmail.com>
2024-08-01 03:55:28 -0700

    Update right-panel.tsx

--- src/components/home/right-panel.tsx
+++ src/components/home/right-panel.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useEffect } from "react";
 import { useQuery } from "convex/react";
-import { api } from "../../../convex/_generated/api";
+//import { api } from "../../../convex/_generated/api";
 import { useConversationStore, ConversationType } from "@/store/chat-store";
 import MessageContainer from "./message-container";
 import MessageInput from "./message-input";
@@ -14,8 +14,8 @@ const RightPanel = ({ conversation }: RightPanelProps) => {
     const { selectedConversation, setSelectedConversation } = useConversationStore();
     
     // Only attempt to fetch messages if a conversation is selected
-    //const messages = useQuery(api.messages.getMessages, { conversation: conversation ? conversation._id : null });
-    const messages = useQuery(api.messages.getMessages, { conversation: conversation?._id });
+    //const messages = useQuery(api.messages.getMessages, { conversation: conversation?._id! });
+
     useEffect(() => {
         if (conversation) {
             setSelectedConversation(conversation);
@@ -30,7 +30,7 @@ const RightPanel = ({ conversation }: RightPanelProps) => {
         <div className='w-full'>
             <div className='flex flex-col h-full'>
                 <div className='flex-grow overflow-hidden'>
-                    <MessageContainer messages={messages} />
+                    <MessageContainer/>
                 </div>
                 <div className='flex-shrink-0 fixed bottom-0 left-0 right-0 z-10 '>
                     <MessageInput conversation={conversation} />
