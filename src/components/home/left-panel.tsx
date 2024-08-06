8074639bbae268beca6bc2ff63c24509bb3257fa
MutebiRoy <roymbscit@gmail.com>
2024-08-06 04:37:31 -0700

    Update left-panel.tsx

--- src/components/home/left-panel.tsx
+++ src/components/home/left-panel.tsx
@@ -12,139 +12,149 @@ import { useConversationStore, ConversationType } from "@/store/chat-store";
 import RightPanel from "./right-panel";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import GroupMembersDialog from "./group-members-dialog";
+//import { Id } from "convex/dist/cjs-types/values/value";
+import { Id } from "../../../convex/_generated/dataModel";
 
 interface LastMessage {
-    _id: string;
-    _creationTime: string;
-    conversation: string;
-    sender: string;
-    content: string;
-    messageType: "image" | "text" | "video";
-  }
+  _id: string;
+  _creationTime: string | number;
+  conversation: string;
+  sender: string;
+  content: string;
+  messageType: "image" | "text" | "video";
+}
 
-  interface Conversation {
-    _id: string;
-    _creationTime: string;
-    lastMessage?: LastMessage;
-    isGroup: boolean;
-    [key: string]: any;  // Additional properties as needed
-  }
+interface Conversation {
+  _id: string;
+  _creationTime: string | number;
+  lastMessage?: LastMessage;
+  isGroup: boolean;
+  participants: string[];
+  [key: string]: any;  // Additional properties as needed
+}
 
-  function convertConversationTypes(conversation: Conversation): Conversation {
+function convertConversationTypes(conversation: Conversation): ConversationType {
     return {
       ...conversation,
+      _id: conversation._id as Id<"conversations">, // Cast to correct Id type
+      _creationTime: conversation._creationTime.toString(),
+      participants: conversation.participants.map(id => id as Id<"users">), // directly map IDs
       lastMessage: conversation.lastMessage ? {
         ...conversation.lastMessage,
-        _creationTime: conversation.lastMessage._creationTime.toString() // Convert to string if not already
+        _id: conversation.lastMessage._id as Id<"messages">, // Ensure this casts to the correct Id type
+        conversation: conversation.lastMessage.conversation as Id<"conversations">, // Ensure this casts to the correct Id type
+        _creationTime: conversation.lastMessage._creationTime.toString(),
+        sender: conversation.lastMessage.sender,
+        content: conversation.lastMessage.content,
+        messageType: conversation.lastMessage.messageType,
       } : undefined
     };
   }
-  
 
 
 
 const LeftPanel = () => {
-    const { isAuthenticated, isLoading } = useConvexAuth();
-    const conversations = useQuery(api.conversations.getMyConversations, isAuthenticated ? undefined : "skip");
-    const { selectedConversation, setSelectedConversation } = useConversationStore();
-    const [isViewingConversation, setIsViewingConversation] = useState(false);
-    const conversationName = selectedConversation?.groupName || selectedConversation?.name || "No conversation selected";
-    const conversationImage = selectedConversation?.groupImage || selectedConversation?.image || "/default-avatar.png";
+  const { isAuthenticated, isLoading } = useConvexAuth();
+  const conversations = useQuery(api.conversations.getMyConversations, isAuthenticated ? undefined : "skip");
+  const { selectedConversation, setSelectedConversation } = useConversationStore();
+  const [isViewingConversation, setIsViewingConversation] = useState(false);
+  const conversationName = selectedConversation?.groupName || selectedConversation?.name || "No conversation selected";
+  const conversationImage = selectedConversation?.groupImage || selectedConversation?.image || "/default-avatar.png";
 
-    useEffect(() => {
-        const conversationIds = conversations?.map((conversation) => conversation._id);
-        if (selectedConversation && conversationIds && !conversationIds.includes(selectedConversation._id)) {
-            setSelectedConversation(null);
-        }
-    }, [conversations, selectedConversation, setSelectedConversation]);
+  useEffect(() => {
+    const conversationIds = conversations?.map((conversation) => conversation._id);
+    if (selectedConversation && conversationIds && !conversationIds.includes(selectedConversation._id)) {
+      setSelectedConversation(null);
+    }
+  }, [conversations, selectedConversation, setSelectedConversation]);
 
-    if (isLoading) return null;
+  if (isLoading) return null;
 
-    const handleBackClick = () => {
-        setIsViewingConversation(false);
-        setSelectedConversation(null);
-    };
+  const handleBackClick = () => {
+    setIsViewingConversation(false);
+    setSelectedConversation(null);
+  };
 
-    const handleConversationClick = (conversation) => {
-        setSelectedConversation(convertConversationTypes(conversation));
-        setIsViewingConversation(true);
-      };
+  const handleConversationClick = (conversation: Conversation) => {
+    setSelectedConversation(convertConversationTypes(conversation));
+    setIsViewingConversation(true);
+  };
 
-    return (
-        <div className='w-full overflow-hidden h-screen'>
-            <div className='fixed top-0 left-0 right-0 bg-left-panel z-10 '>
-                <div className='flex justify-between bg-gray-primary p-3'>
-                    {isViewingConversation ? (
-                    <div className='flex items-center'>
-                        <button onClick={handleBackClick}>
-                            <ChevronLeft size={24} />
-                        </button>
-                        <Avatar className='ml-4'>
-                            <AvatarImage src={conversationImage || "/placeholder.png"} className='object-cover' />
-                            <AvatarFallback>
-                            <div className='animate-pulse bg-gray-tertiary w-full h-full rounded-full' />
-                            </AvatarFallback>
-                        </Avatar>
-                        <div className='flex flex-col ml-4'>
-                            <p>{conversationName}</p>
-                            {selectedConversation && selectedConversation.isGroup && (
-                                <GroupMembersDialog selectedConversation={selectedConversation} />
-                            )}
-                        </div>
-                    </div>
-                    ) : (
-                        <UserButton />
-                    )}
-                    <div className='flex items-center gap-3'>
-                        {isAuthenticated && <UserListDialog />}
-                        <ThemeSwitch />
-                    </div>
-                </div>
-                {!isViewingConversation && (
-                <div className='p-3 flex items-center'>
-                    <div className='relative h-10 mx-3 flex-1'>
-                    <Search
-                        className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10'
-                        size={18}
-                    />
-                    <Input
-                        type='text'
-                        placeholder='Search or start a new chat'
-                        className='pl-10 py-2 text-sm w-full rounded shadow-sm bg-gray-primary focus-visible:ring-transparent'
-                    />
-                    </div>
-                </div>
+  return (
+    <div className='w-full overflow-hidden h-screen'>
+      <div className='fixed top-0 left-0 right-0 bg-left-panel z-10 '>
+        <div className='flex justify-between bg-gray-primary p-3'>
+          {isViewingConversation ? (
+            <div className='flex items-center'>
+              <button onClick={handleBackClick}>
+                <ChevronLeft size={24} />
+              </button>
+              <Avatar className='ml-4'>
+                <AvatarImage src={conversationImage || "/placeholder.png"} className='object-cover' />
+                <AvatarFallback>
+                  <div className='animate-pulse bg-gray-tertiary w-full h-full rounded-full' />
+                </AvatarFallback>
+              </Avatar>
+              <div className='flex flex-col ml-4'>
+                <p>{conversationName}</p>
+                {selectedConversation && selectedConversation.isGroup && (
+                  <GroupMembersDialog selectedConversation={selectedConversation} />
                 )}
+              </div>
             </div>
-            <div className={`my-3 flex flex-col gap-0 overflow-auto h-full pb-[44px] ${!isViewingConversation ? 'pt-[114px]' : 'pt-[78px]'}`}>
-            {!isViewingConversation &&
-                conversations?.map((conversation, index) => (
-                    <div className={index === 0 ? 'pt-[0px]' : ''} key={conversation._id}>
-                        <Conversation
-                            key={conversation._id}
-                            conversation={convertConversationTypes(conversation)}
-                            onClick={() => handleConversationClick(conversation)}
-                        />
-                    </div>
-                ))
-            }
-
-                {isViewingConversation && selectedConversation && (
-                <div className='overflow-auto h-full'>
-                    <RightPanel conversation={selectedConversation} />
-                </div>
-                )}
-                {conversations?.length === 0 && !isViewingConversation && (
-                <>
-                    <p className='text-center text-gray-500 text-sm mt-3'>No conversations yet!</p>
-                    <p className='text-center text-gray-500 text-sm mt-3'>
-                        Click on any name to start a conversation
-                    </p>
-                </>
-                )}
-            </div>
+          ) : (
+            <UserButton />
+          )}
+          <div className='flex items-center gap-3'>
+            {isAuthenticated && <UserListDialog />}
+            <ThemeSwitch />
+          </div>
         </div>
-    );
+        {!isViewingConversation && (
+          <div className='p-3 flex items-center'>
+            <div className='relative h-10 mx-3 flex-1'>
+              <Search
+                className='absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10'
+                size={18}
+              />
+              <Input
+                type='text'
+                placeholder='Search or start a new chat'
+                className='pl-10 py-2 text-sm w-full rounded shadow-sm bg-gray-primary focus-visible:ring-transparent'
+              />
+            </div>
+          </div>
+        )}
+      </div>
+      <div className={`my-3 flex flex-col gap-0 overflow-auto h-full pb-[44px] ${!isViewingConversation ? 'pt-[114px]' : 'pt-[78px]'}`}>
+        {!isViewingConversation &&
+          conversations?.map((conversation, index) => (
+            <div className={index === 0 ? 'pt-[0px]' : ''} key={conversation._id}>
+              <Conversation
+                key={conversation._id}
+                conversation={convertConversationTypes(conversation)}
+                onClick={() => handleConversationClick(conversation)}
+              />
+            </div>
+          ))
+        }
+
+        {isViewingConversation && selectedConversation && (
+          <div className='overflow-auto h-full'>
+            <RightPanel conversation={selectedConversation} />
+          </div>
+        )}
+        {conversations?.length === 0 && !isViewingConversation && (
+          <>
+            <p className='text-center text-gray-500 text-sm mt-3'>No conversations yet!</p>
+            <p className='text-center text-gray-500 text-sm mt-3'>
+              Click on any name to start a conversation
+            </p>
+          </>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default LeftPanel;
