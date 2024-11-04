2750b79358a0e671b89bb3ca7916b842721ab67d
MutebiRoy <roymbscit@gmail.com>
2024-11-04 02:09:51 -0800

    Update schema.ts

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,26 +2,39 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-	users: defineTable({
-		name: v.optional(v.string()),
-		email: v.string(),
-		image: v.string(),
-		tokenIdentifier: v.string(),
-		isOnline: v.boolean(),
-	}).index("by_tokenIdentifier", ["tokenIdentifier"]),
+  users: defineTable({
+    name: v.optional(v.string()),
+    email: v.string(),
+    image: v.string(),
+    tokenIdentifier: v.string(),
+    isOnline: v.boolean(),
+  })
+    .index("by_tokenIdentifier", ["tokenIdentifier"])
+    // Add this line to define a search index on the 'name' field
+    .searchIndex("search_name", { searchField: "name" }),
 
-	conversations: defineTable({
-		participants: v.array(v.id("users")),
-		isGroup: v.boolean(),
-		groupName: v.optional(v.string()),
-		groupImage: v.optional(v.string()),
-		admin: v.optional(v.id("users")),
-	}),
+  conversations: defineTable({
+    isGroup: v.boolean(),
+    groupName: v.optional(v.string()),
+    groupImage: v.optional(v.string()),
+    admin: v.optional(v.id("users")),
+  }),
 
-	messages: defineTable({
-		conversation: v.id("conversations"),
-		sender: v.string(), // should be string so that it doesn't throw errors in openai part ("ChatGPT")
-		content: v.string(),
-		messageType: v.union(v.literal("text"), v.literal("image"), v.literal("video")),
-	}).index("by_conversation", ["conversation"]),
+  user_conversations: defineTable({
+    user: v.id("users"),
+    conversation: v.id("conversations"),
+  })
+    .index("by_user", ["user"])
+    .index("by_conversation", ["conversation"]),
+
+  messages: defineTable({
+    content: v.string(),
+    conversation: v.id("conversations"),
+    sender: v.id("users"), // Defines 'sender' as an ID referencing 'users' table
+    messageType: v.union(
+      v.literal("text"), 
+      v.literal("image"), 
+      v.literal("video")
+    ),
+  }).index("by_conversation", ["conversation"]),
 });
