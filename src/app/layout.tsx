297142f04118ba66b6554f8a42cd8cca7cf8fd50
MutebiRoy <roymbscit@gmail.com>
2024-11-14 01:43:02 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+import { useEffect } from "react";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import ConvexClientProvider from "@/providers/convex-client-provider";
 import { Toaster } from "react-hot-toast";
+import ViewportHeightSetter from '@/components/home/ViewportHeightSetter';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,14 +19,12 @@ export const metadata: Metadata = {
 };
 
 
-export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
+			<ViewportHeightSetter /> {/* Include the Client Component here */}
 				<ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
 					<ConvexClientProvider>
 						{children}
