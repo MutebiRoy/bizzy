723bdce968ea64af0c0892836273ae2d83ffdecc
MutebiRoy <roymbscit@gmail.com>
2024-11-14 00:32:16 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import LeftPanel from "@/components/home/left-panel";
 import RightPanel from "@/components/home/right-panel";
 import { useTheme } from "next-themes";
-
 export default function Home() {
 	return (
-		<main className=''>
-			<div className='overflow-y-hidden h-screen mx-auto bg-left-panel'>
+		<main className='w-full h-full overflow-hidden'>
+			{/* <div className='overflow-y-hidden h-screen mx-auto bg-left-panel'> */}
+			<div className='h-screen mx-auto bg-left-panel'>
 				<LeftPanel />
 			</div>
 		</main>
