e254c719c6b512673f4b4ad4b71add0297442eac
MutebiRoy <roymbscit@gmail.com>
2024-11-20 15:59:09 -0800

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config = {
 			},
 		},
 		extend: {
+			fontSize: {
+				input: '16px', // Add this line
+			  },
 			colors: {
 				border: "hsl(var(--border))",
 				input: "hsl(var(--input))",
