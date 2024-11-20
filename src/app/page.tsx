db4d6e7627669b3cc55f8141e589aa99f079210a
MutebiRoy <roymbscit@gmail.com>
2024-11-20 01:05:57 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,8 @@ import { useTheme } from "next-themes";
 
 export default function Home() {
 	return (
-		<main className='flex flex-col h-screen'>
-			{/* <div className='overflow-y-hidden h-screen mx-auto bg-left-panel bo'> */}
-				<LeftPanel />
-			{/* </div> */}
-		</main>
+		<div className="flex flex-col h-screen">
+			<LeftPanel />
+		</div>
 	);
 }
