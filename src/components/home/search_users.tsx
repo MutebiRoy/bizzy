01946c5859529915f355c3080c60535dd1e3ec61
MutebiRoy <roymbscit@gmail.com>
2024-11-20 01:18:25 -0800

    Update search_users.tsx

--- src/components/home/search_users.tsx
+++ src/components/home/search_users.tsx
@@ -99,16 +99,16 @@ const SearchUsers = () => {
 
   return (
     <div className="relative">
-      <Search
-        className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-500 z-10" size={18}
-      />
+      <div className="relative">
+      <Search className="w-5 h-5 text-gray-400 absolute left-3 top-1/2 transform -translate-y-1/2" />
       <Input
         type="text"
         placeholder="Search users..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        className="w-full pl-10 px-3 py-2 text-sm rounded shadow-sm bg-gray-primary focus-visible:ring-transparent"
+        className="w-full pl-20 px-3 py-2 border rounded-md focus:outline-none focus:ring focus:border-primary"
       />
+      </div>
       {isAuthenticated && trimmedSearchTerm && searchResults && (
         <div className="absolute mt-1 w-full bg-background border rounded shadow z-20">
           {searchResults.map((user: UserType) => (
