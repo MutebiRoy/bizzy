6307de0c4830e789267e33a637d9fc10c959a33e
MutebiRoy <roymbscit@gmail.com>
2024-11-20 01:05:41 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,27 +5,29 @@ import "./globals.css";
 import { ThemeProvider } from "@/providers/theme-provider";
 import ConvexClientProvider from "@/providers/convex-client-provider";
 import { Toaster } from "react-hot-toast";
-import ViewportHeightSetter from '@/components/home/ViewportHeightSetter';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
 	title: "Bizmous",
 	description: "Get paid to chat",
-	viewport: {
-	  width: "device-width",
-	  initialScale: 1.0
-	},
+	// viewport: {
+	//   width: "device-width",
+	//   initialScale: 1.0
+	// },
 };
-
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   
 	return (
 		<html lang='en'>
 			<body className={inter.className}>
-			<ViewportHeightSetter /> {/* Include the Client Component here */}
-				<ThemeProvider attribute='class' defaultTheme='system' enableSystem disableTransitionOnChange>
+				<ThemeProvider 
+					attribute='class' 
+					defaultTheme='system' 
+					enableSystem 
+					disableTransitionOnChange>
+						
 					<ConvexClientProvider>
 						{children}
 						<Toaster />
