408064895b0517887b8ccc6e978a3d1b357eb05e
MutebiRoy <roymbscit@gmail.com>
2024-11-21 17:34:02 -0800

    Update left-panel.tsx

--- src/components/home/left-panel.tsx
+++ src/components/home/left-panel.tsx
@@ -221,8 +221,9 @@ const LeftPanel = () => {
                 </p>
               </>
             )}
+          </div>
 
-<footer className="flex-none flex-shrink-0 z-20 h-16">
+          <footer className="flex-none flex-shrink-0 z-20 h-16">
             <div className="p-4 flex space-x-4">
               {/* Home Button */}
               {/* <button
@@ -242,9 +243,6 @@ const LeftPanel = () => {
               </button>
             </div>
           </footer> 
-          </div>
-
-          
 
         </>
       ) : (
