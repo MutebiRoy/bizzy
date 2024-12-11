0f3c1190a6960e52048d3718554e580dc98621d4
MutebiRoy <roymbscit@gmail.com>
2024-12-10 22:59:05 -0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	images: {
-		remotePatterns: [
-			{ 
-				protocol: 'https',
-				hostname: 'good-anteater-372.convex.cloud',
-				port: '',
-				pathname: '/api/storage/**',
-			}
-		],
-	},
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'good-anteater-372.convex.cloud',
+        port: '',
+        pathname: '/api/storage/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'img.clerk.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
 };
 
 export default nextConfig;
