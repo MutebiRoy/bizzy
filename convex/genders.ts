b351d6336546a658022aea21ee39a1dcc50cbcc4
MutebiRoy <roymbscit@gmail.com>
2024-12-10 17:27:00 -0800

    Update genders.ts

--- convex/genders.ts
+++ convex/genders.ts
@@ -1,18 +1,1 @@
-// convex\users.ts
-import { ConvexError, v } from "convex/values";
-import { internalMutation, mutation, query } from "./_generated/server";
-import { Id } from "./_generated/dataModel";
 
-
-export const getAllGenders = query(async ({ db }) => {
-	const genderDocs = await db.query("genders").collect();
-	// genderDocs: [{_id, genderName}, ...]
-  
-	// Extract genderName strings
-	const genderNames = genderDocs.map(d => d.genderName);
-  
-	// Make sure we have unique set
-	const uniqueGenders = Array.from(new Set(genderNames));
-  
-	return uniqueGenders;
-});
