b1e652155c8205a0d920b6fd1ee1a842a28e25c8
MutebiRoy <roymbscit@gmail.com>
2024-12-16 15:55:34 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,6 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
 	title: "Bizmous",
 	description: "Get paid to chat",
-	//viewport: "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no, viewport-fit=cover",
 };
 
 export const viewport: Viewport = {
