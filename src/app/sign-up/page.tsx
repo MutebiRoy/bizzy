5ca69fc08282549566b58658c998ee1a71056a6b
MutebiRoy <roymbscit@gmail.com>
2024-12-31 02:07:36 -0800

    Update page.tsx

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -1,14 +1,14 @@
-// app/sign-up/page.tsx
 "use client";
 import { SignUp } from "@clerk/nextjs";
 
 export default function SignUpPage() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <SignUp 
-        path="/sign-up" 
-        routing="path" 
-        signInUrl="/sign-in" 
+      <SignUp
+        path="/sign-up"
+        routing="path"
+        signInUrl="/sign-in"
+        forceRedirectUrl="/" // Always go to the homepage after sign up
       />
     </main>
   );
