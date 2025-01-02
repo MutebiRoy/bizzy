f43a0c3d2fa06c4a26ee1349c3828ec57f0a5c53
MutebiRoy <roymbscit@gmail.com>
2025-01-02 04:39:35 -0800

    Update page.tsx

--- src/app/sign-up/page.tsx
+++ src/app/sign-up/page.tsx
@@ -8,7 +8,7 @@ export default function SignUpPage() {
         path="/sign-up"
         routing="path"
         signInUrl="/sign-in"
-        forceRedirectUrl="/" // Always go to the homepage after sign up
+        // forceRedirectUrl="/" // Always go to the homepage after sign up
       />
     </main>
   );
