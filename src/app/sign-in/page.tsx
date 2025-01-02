12b92da6db4b29171a9b7e90bebd0bc185d00e00
MutebiRoy <roymbscit@gmail.com>
2025-01-02 04:50:56 -0800

    Update page.tsx

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -1,10 +1,15 @@
 "use client";
-import { SignInButton } from "@clerk/clerk-react";
+import { SignIn } from "@clerk/nextjs";
 
 export default function SignInPage() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <SignInButton />
+      <SignIn
+        path="/sign-in"
+        routing="path"
+        signUpUrl="/sign-up"
+        forceRedirectUrl="/" // Always go to the homepage after sign in
+      />
     </main>
   );
 }
