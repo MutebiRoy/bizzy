5c1fd7789c8dd7a056b94b94656f1bf3f27bbeda
MutebiRoy <roymbscit@gmail.com>
2025-01-02 04:45:04 -0800

    Update page.tsx

--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -4,12 +4,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInPage() {
   return (
     <main className="flex flex-col items-center justify-center h-screen">
-      <SignIn
-        path="/sign-in"
-        routing="path"
-        signUpUrl="/sign-up"
-        // forceRedirectUrl="/"
-      />
+      <SignInButton />
     </main>
   );
 }
