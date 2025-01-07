ed77c7f09f2f2d2b04d55496a58630b66b1886ee
MutebiRoy <roymbscit@gmail.com>
2025-01-06 20:11:04 -0800

    Update auth.config.ts

--- convex/auth.config.ts
+++ convex/auth.config.ts
@@ -1,19 +1,4 @@
-// C:\Users\mutebi\Desktop\bizmous\convex\auth.config.ts
-// const authConfig = {
-// 	providers: [
-// 	  {
-// 		//domain: process.env.CLERK_ISSUER_URL,
-// 		domain: "https://hopeful-whale-77.clerk.accounts.dev",
-// 		applicationID: "convex",
-// 	  },
-// 	]
-//   };
-
-// export default authConfig;
-
-
-
-// C:\Users\mutebi\Desktop\bizmous\convex\auth.config.ts
+// convex\auth.config.ts
 const authConfig = {
 	providers: [
 	  {
