ed83b9d2875f0cc3bddb8a182e4de3ddaf7f2c2a
MutebiRoy <roymbscit@gmail.com>
2025-01-06 18:04:19 -0800

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,22 @@
-// src/middleware.ts
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
-
-const isProtectedRoute = createRouteMatcher([
-	// Protect all routes EXCEPT sign-in, sign-up, static files, _next, etc.
-	"/((?!_next|sign-in|sign-up|.*\\..*).*)",
-]);
-
-export default clerkMiddleware((auth, req) => {
+// src\middleware.ts"
+import {
+	clerkMiddleware,
+	createRouteMatcher
+  } from '@clerk/nextjs/server';
+  
+  const isProtectedRoute = createRouteMatcher([
+	'/(.*)'
+  ]);
+  
+  export default clerkMiddleware((auth, req) => {
 	if (!auth().userId && isProtectedRoute(req)) {
-	  // If user is not signed in and route is protected, redirect to sign-in:
-	  const signInUrl = new URL("/sign-in", req.url);
-	  return NextResponse.redirect(signInUrl);
+  
+	  // Add custom logic to run before redirecting
+  
+	  return auth().redirectToSignIn();
 	}
-});
-
-export const config = {
-	matcher: ["/((?!_next|.*\\..*).*)"],
-};
+  });
+  
+  export const config = {
+	matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+  };
