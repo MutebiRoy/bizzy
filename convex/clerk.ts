cc5c9b5a7724aaee6d1bfb3c0334b7749f5b4031
MutebiRoy <roymbscit@gmail.com>
2025-01-09 03:08:05 -0800

    Update clerk.ts

--- convex/clerk.ts
+++ convex/clerk.ts
@@ -1,24 +1,116 @@
-"use node";
+// convex\http.ts
+import { httpRouter } from "convex/server";
+import { httpAction } from "./_generated/server";
+import { internal } from "./_generated/api";
 
-import type { WebhookEvent } from "@clerk/clerk-sdk-node";
-import { v } from "convex/values";
 
-import { Webhook } from "svix";
+const http = httpRouter();
 
-import { internalAction } from "./_generated/server";
+http.route({
+	path: "/clerk",
+	method: "POST",
+	handler: httpAction(async (ctx, req) => {
+		const payloadString = await req.text();
+		const headerPayload = req.headers;
 
-const WEB_HOOK_SECRET = process.env.CLERK_WEBHOOK_SECRET as string;
+		try {
+			const result = await ctx.runAction(internal.clerk.fulfill, {
+				payload: payloadString,
+				headers: {
+					"svix-id": headerPayload.get("svix-id")!,
+					"svix-signature": headerPayload.get("svix-signature")!,
+					"svix-timestamp": headerPayload.get("svix-timestamp")!,
+				},
+			});
 
-export const fulfill = internalAction({
-	args: {
-		headers: v.any(),
-		payload: v.string(),
-	},
-	handler: async (ctx, args) => {
-		const wh = new Webhook(WEB_HOOK_SECRET);
-		const payload = wh.verify(args.payload, args.headers) as WebhookEvent;
-		return payload;
-	},
+			switch (result.type) {
+				case "user.created":
+
+					const email = result.data.email_addresses[0]?.email_address;
+					let defaultName = email.split('@')[0];
+
+					const obfuscateName = (name: string) => {
+						const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+						const randomChar = () => characters.charAt(Math.floor(Math.random() * characters.length));
+						let obfuscatedName = '';
+						for (let i = 0; i < name.length; i++) {
+							obfuscatedName += name[i];
+							if (Math.random() > 0.5) {
+								obfuscatedName += randomChar();
+							}
+						}
+						return obfuscatedName;
+					};
+
+					defaultName = obfuscateName(defaultName);
+
+					let name = "";
+					if (result.data.first_name && result.data.last_name) {
+						name = `${result.data.first_name} ${result.data.last_name}`;
+					} else if (result.data.first_name) {
+						name = result.data.first_name;
+					} else if (result.data.last_name) {
+						name = result.data.last_name;
+					} else {
+						name = defaultName;
+					}
+
+					// User Signup Email notification to admin
+					//const tokenIdentifier = `${process.env.CLERK_APP_DOMAIN}|${result.data.id}`;
+					const tokenIdentifier = result.data.id;
+					
+					const userCreateResult = await ctx.runMutation(internal.users.createUser, {
+						tokenIdentifier,
+						email: email,
+						name: name,
+						image: result.data.image_url,
+					});
+
+					if (userCreateResult.status === "CREATED") {
+						// This is the user's very first time
+						// 1. Send an internal email if needed
+						await ctx.runAction(internal.clerk.sendNewUserEmail, {
+							name,
+							email: email,
+							clerkId: result.data.id,
+						});
+					}
+					
+					break;
+				case "user.updated":
+					await ctx.runMutation(internal.users.updateUser, {
+						//tokenIdentifier: `${process.env.CLERK_APP_DOMAIN}|${result.data.id}`,
+						tokenIdentifier: result.data.id,
+						image: result.data.image_url,
+					});
+					break;
+				case "session.created":
+					await ctx.runMutation(internal.users.setUserOnline, {
+						//tokenIdentifier: `${process.env.CLERK_APP_DOMAIN}|${result.data.user_id}`,
+						tokenIdentifier: result.data.id,
+					});
+					break;
+				case "session.ended":
+					await ctx.runMutation(internal.users.setUserOffline, {
+						//tokenIdentifier: `${process.env.CLERK_APP_DOMAIN}|${result.data.user_id}`,
+						tokenIdentifier: result.data.id,
+					});
+					break;
+			}
+
+			return new Response(null, {
+				status: 200,
+			});
+		} catch (error) {
+			console.log("Webhook Error🔥🔥", error);
+			return new Response("Webhook Error", {
+				status: 400,
+			});
+		}
+	}),
 });
 
-// https://docs.convex.dev/functions/internal-functions
+export default http;
+
+// https://docs.convex.dev/functions/http-actions
+// Internal functions can only be called by other functions and cannot be called directly from a Convex client.
